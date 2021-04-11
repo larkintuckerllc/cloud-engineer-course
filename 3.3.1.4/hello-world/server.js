908f018b40ee67209943df8b6a2514ca67b3dd84
John Tucker <jtucker@spotify.com>
2021-04-10 21:19:02 -0400

    done

--- 3.3.1.4/hello-world/server.js
+++ 3.3.1.4/hello-world/server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World v2!')
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
